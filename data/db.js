03050153522641dda3eab22cc4c9611eada3f4b6
soundarya48 <soundaryafocus@gmail.com>
2026-02-05 23:31:18 +0530

    Add new

--- data/db.js
+++ data/db.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql2');
+require('dotenv').config(); 
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password:"tiger",
-  database:"college"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-connection.connect((err)=>{
-    if(err){
-        console.log('Database connection failed', err);
-        return;
-    }
-    console.log('Database connected successfully'); 
-
+connection.connect((err) => {
+  if (err) {
+    console.log('❌ Database connection failed:', err.message);
+    return;
+  }
+  console.log('✅ Database connected successfully');
 });
 
 module.exports = connection;
-
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.json());
 
 const collegeRoutes = require('./route/collegeroute');  
-app.use('/colleges', collegeRoutes);    
+app.use('/colleges', collegeRoutes);  
+  
 
 app.listen(3000, () => {    
     console.log('Server is running on port 3000');
